f0bdc9225b50e271216fbfc4ecf47a5cf9b493ab
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-09 18:27:20 +0530

    learning variables concept

---
+++ variable.js
@@ -0,0 +1,39 @@
+// {
+//     var a= 10
+// }
+// console.log(a)
+
+
+// var a= 10
+
+// a=20
+// console.log(a);
+
+
+// let a // initialization
+// a=20
+// console.log(a);
+
+
+
+
+
+// var price =100
+// var product = "iphone"
+// var tax =10
+// console.log(product);
+// var total =price+tax
+
+// console.log(total); 
+
+
+// var fruitName ="apple"
+// var count =10
+// var price =100
+// var total = count*price
+// console.log("Answer is",total);
+
+
+{
+    
+}
