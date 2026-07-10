cbc7892841e4953dbe75d3462d70376d2949d752
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-10 18:57:05 +0530

    some ex problem solved

---
+++ function.js
@@ -0,0 +1,50 @@
+
+{
+    
+    function abc()
+{
+    console.log("hello");
+    
+}
+abc()
+
+}
+
+
+// ex for function calling
+
+{
+
+    function oppo()
+    {
+        console.log("this is oppo phone");
+        
+    }
+    
+    function samsung()
+    {
+
+        console.log("This is samsung phone");
+        
+    }
+
+    function apple()
+    {
+        console.log("This is apple phone");
+        
+    }
+
+    function redmi() {
+        console.log("this is redmi phone");
+        
+    }
+
+    oppo()
+    samsung()
+    apple()
+    redmi()
+
+}
+
+
+
