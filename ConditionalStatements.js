5d57c3b47ace17f994c3c2a5acdc252781e238ba
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-11 21:34:10 +0530

    feat: added self-practice exercise for voting eligibility logic

---
+++ ConditionalStatements.js
@@ -0,0 +1,40 @@
+// Conditional statements
+
+if (false)
+{
+    console.log("True");
+    
+}
+
+else{
+    console.log(" false");
+    
+}
+
+
+
+{
+// If Else Exercise
+
+// 1. Question: Did you finish your homework?
+// -If the answer is "yes" print great job
+// -if the answer is no print the "finish your homework before playing"
+
+
+console.clear(); 
+ 
+    let homeworkStatus = "no"; 
+    if (homeworkStatus === "yes") {
+        console.log("Great job!");
+    } else {
+        console.log("Finish your homework before playing.");
+    }
+
+}
+
+
+
+
+
+
+
