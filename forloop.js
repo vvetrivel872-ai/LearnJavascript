b451674a9d7a5ce8286453e7129d9f8b33fb4a43
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-13 18:00:26 +0530

    feat: practice for loops with step increments, reverse counting, and multiplication table generation

---
+++ forloop.js
@@ -0,0 +1,19 @@
+for (count=1; count<=10; count=count+2){
+    console.log(count);
+    
+}
+
+console.clear()
+
+for(count=10; count>=1; count=count-1)
+{
+    console.log(count)
+}
+
+console.clear()
+
+for (i=1; i<=10; i=i+1)
+{
+    console.log(i+"x2="+i*2);
+    
+}
