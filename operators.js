5ba9d3af2b8efb34207de3a52aabdbda36e4b219
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-13 15:14:26 +0530

    feat: practice logical operators, ternary operator, and conditional if-else-if statements

---
+++ operators.js
@@ -0,0 +1,54 @@
+console.log(true && false);
+
+console.clear()
+
+console.log(!true);
+
+
+
+
+console.clear()
+
+
+{
+// Ternary Operator
+
+let age = 16
+
+let type =age>18? "Adult Ticket" : "chile ticket "
+
+console.log(type);
+}
+
+
+console.clear()        
+
+
+var  score =60
+
+if(score <=50){
+    console.log("you need to improve");
+    
+    
+}
+
+
+
+else if (score>50 && score<=70){
+
+   
+    console.log("Good job");
+    
+}
+
+else if (score>70)
+
+    {
+        console.log("Exelent perfomence");
+        
+    }
+
+else{
+    console.log("Fail");
+    
+}
