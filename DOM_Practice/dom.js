e6ead66f45f18c3465935c4bf039ba6685813332
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-14 13:31:04 +0530

    feat: practice DOM textContent to read and update element text

--- DOM_Practice/dom.js
+++ DOM_Practice/dom.js
@@ -1,1 +1,12 @@
-console.log("dom file connected");
+
+
+var para=document.getElementById("one")
+
+console.log(para.textContent); //Used to print only the text content inside that specific element
+
+para.textContent="bye"  //Used to change the text inside the element 
+
+console.log(para)
+
+
+
