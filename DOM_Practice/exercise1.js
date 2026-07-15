f0f524f6468214235c38fa9b2152502491a07142
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-07-15 13:11:41 +0530

    feat: build basic calculator to add two numbers with input conversion

---
+++ DOM_Practice/exercise1.js
@@ -0,0 +1,13 @@
+var num1=document.getElementById("num1")
+var num2=document.getElementById("num2")
+var op=document.getElementById("op")
+
+function btn()
+{
+    tot= Number(num1.value)+Number(num2.value)//HTML inputs return values as Strings , Number () used to convert string into string into number
+    op.textContent=tot
+
+  
+}
+
+
