1665da71f5c3b626b38fe598a2c08b3ec85effa0
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-08-02 17:15:53 +0530

    feat: add click event to render h1 tag

--- DOM_Practice/changeStyle_ex.js
+++ DOM_Practice/changeStyle_ex.js
@@ -1,8 +1,20 @@
-var inputbox=document.getElementById("inputbox")
-var result=document.getElementById("result")
+// var inputbox=document.getElementById("inputbox")
+// var result=document.getElementById("result")
 
 
-function update(){
-    result.textContent=inputbox.value
+// function update(){
+//     result.textContent=inputbox.value
   
+// }
+
+
+
+
+
+
+var div=document.getElementById("result")
+function update()
+{   var listItem=document.createElement("h1")
+    listItem.textContent="Hello"
+    div.append(listItem)
 }
