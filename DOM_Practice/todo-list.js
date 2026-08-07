dc2bb0f95aa8b7ec68faa54d9ad5fb2d78f77cfa
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-08-07 21:53:27 +0530

    Printed h1 textContent using for loop and querySelectorAll

---
+++ DOM_Practice/todo-list.js
@@ -0,0 +1,16 @@
+
+// var fruit=["apple","orange","banana","graph",]
+
+// for (count=0; count<fruit.length; count=count+1) {
+
+//         console.log(fruit[count]+count);
+
+// }
+
+var div=document.querySelectorAll("h1")
+
+for(count=0; count<div.length; count=count+1) {
+    
+    console.log(div[count].textContent);
+    
+}
