2d3ac977574635fe84972189c5be19d1d1c0012f
vvetrivel872-ai <vvetrivel872@gmail.com>
2026-08-14 14:18:16 +0530

    Practice DOM insertion methods using append, prepend, and insertAdjacentElement

--- DOM_Practice/todo-list.js
+++ DOM_Practice/todo-list.js
@@ -1,4 +1,6 @@
 
+// Exercise 1: Array Iteration using For Loop
+
 // var fruit=["apple","orange","banana","graph",]
 
 // for (count=0; count<fruit.length; count=count+1) {
@@ -7,10 +9,36 @@
 
 // }
 
-var div=document.querySelectorAll("h1")
 
-for(count=0; count<div.length; count=count+1) {
+
+
+// Exercise 2: Selecting & Printing DOM Elements
+
+// var div=document.querySelectorAll("h1")
+
+// for(count=0; count<div.length; count=count+1) {
     
-    console.log(div[count].textContent);
+//     console.log(div[count].textContent);
     
-} 
+// } 
+
+
+// append and prepend
+
+// var div=document.querySelector("div")
+// div.append("hello")
+// div.prepend("hi")
+
+
+
+//inserAdjestElement
+
+var div=document.querySelector("div")
+var h1=document.createElement("h1")
+h1.textContent="vetri"
+div.insertAdjacentElement("afterend",h1)
+
+
+
+
+
